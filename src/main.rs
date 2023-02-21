6a3bb7c5d69b6fab1dd19979670f0470ebb4d4ba
Luj8n <luj8n@duck.com>
2023-02-21 13:25:54 +0200

    started evaluate

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ use std::time::Instant;
 
 use board::Board;
 
-fn main() {
+fn perft_div(fen: &str, depth: usize) {
   let current_time = Instant::now();
 
-  let mut board = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+  let mut board = Board::from_fen(fen);
 
-  const DEPTH: usize = 6;
+  dbg!(board.evaluate());
 
-  fn perft(depth: usize, board: &mut Board) -> u64 {
+  fn perft(cur_depth: usize, board: &mut Board, depth: usize) -> u64 {
     let mut nodes = 0;
 
-    if depth == 0 {
+    if cur_depth == 0 {
       return 1;
     }
 
@@ -25,9 +25,9 @@ fn main() {
     for chess_move in board.pseudo_legal_moves() {
       board.make_move(&chess_move);
       if !board.in_check(&side_to_move) {
-        let to_add = perft(depth - 1, board);
+        let to_add = perft(cur_depth - 1, board, depth);
 
-        if depth == DEPTH {
+        if cur_depth == depth {
           println!("{}: {}", chess_move.to_fen(), to_add);
         }
 
@@ -39,8 +39,8 @@ fn main() {
     nodes
   }
 
-  let nodes = perft(DEPTH, &mut board);
-  println!("Depth = {DEPTH}, total nodes = {nodes}");
+  let nodes = perft(depth, &mut board, depth);
+  println!("Depth = {depth}, total nodes = {nodes}");
 
   let time_elapsed = current_time.elapsed();
   println!("Time taken: {:?}", time_elapsed);
@@ -48,4 +48,8 @@ fn main() {
   let seconds = time_elapsed.as_secs_f64();
 
   println!("Speed: {} Mn/s", ((nodes as f64 / 1_000_000.) / seconds).round())
+}
+
+fn main() {
+  perft_div("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 6);
 }
